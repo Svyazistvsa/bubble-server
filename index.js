4c59d08cfdebdeff445394fd690a7e539fddc3d3
Svyazist <sviazist.vsa@gmail.com>
2024-11-12 22:03:30 +0500

    end

--- index.js
+++ index.js
@@ -3,36 +3,26 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const bodyParser = require('body-parser');  
+const path = require('path');
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:9000/");
-    res.header("Access-Control-Allow-Methods", "GET, POST");
+    res.header("Access-Control-Allow-Origin", "http://localhost:9000");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
 })
+
+app.options('*',(req,res) => {
+    res.sendStatus(200);
+});
+
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res){
     
-    res.send(`<body>
-    <h1>Welcome to HTTPS Server</h1>
-    <br><br>
-    <h3>Enter your message</h3>
-
-    <!--  sending post request to "mssg" with 
-        the message from the textarea -->
-    <form action="mssg" method="post">
-        <textarea name="message" id="" 
-            cols="30" rows="10"></textarea>
-        <button type="submit">Send</button>
-    </form>
-</body>`);
-});
-
-app.post("/mssg", function (req, res){
-    console.log(req.body);
-    res.redirect('/');
+    res.sendFile(path.join(__dirname, '/content/soap.html'));
 });
 
 const options = {
