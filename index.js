9f85f99e0128646f643f7082f940ccfb0bd097f0
Svyazist <sviazist.vsa@gmail.com>
2024-11-12 16:40:44 +0500

    CORS is work

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const app = express();
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:9000/");
+    res.header("Access-Control-Allow-Metods", "GET, POST");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+})
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res){
+    
     res.sendFile(__dirname, '/index.html');
 });
 
 app.post("/mssg", function (req, res){
     console.log(req.body);
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:9000/main.bundle.js");
     res.redirect('/');
 });
 
